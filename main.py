7b653bb2dd1b6bab84177c7f3bb5b61c4fca6b89
Andriy <andrijbuluk@gmail.com>
2026-03-03 16:05:09 +0100

    Genres and actors

--- main.py
+++ main.py
@@ -2,6 +2,95 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+
+
+# Genre
+def create_genre(name: str) -> None:
+    Genre.objects.create(name=name)
+
+
+def update_genre(genre_id: int, genre_name: str) -> None:
+    Genre.objects.filter(id=genre_id).update(name=genre_name)
+
+
+def all_genres() -> QuerySet:
+    return Genre.objects.all()
+
+
+def delete_genre(genre_id: int) -> None:
+    Genre.objects.filter(id=genre_id).delete()
+
+
+# Actor
+def create_actor(first_name: str, last_name: str) -> None:
+    Actor.objects.create(first_name=first_name, last_name=last_name)
+
+
+def update_actor(actor_id: int, first_name: str, last_name: str) -> None:
+    data = {}
+    if first_name:
+        data["first_name"] = first_name
+    if last_name:
+        data["last_name"] = last_name
+    if data:
+        Actor.objects.filter(id=actor_id).update(**data)
+
+
+def all_actors() -> QuerySet:
+    return Actor.objects.all()
+
+
+def delete_actor(actor_id: int) -> None:
+    Actor.objects.filter(id=actor_id).delete()
+
 
 def main() -> QuerySet:
-    pass
+
+    # Create genres
+    create_genre("Western")
+    create_genre("Action")
+    create_genre("Dramma")
+
+    # Create actors
+    create_actor("George", "Klooney")
+    create_actor("Kianu", "Reaves")
+    create_actor("Will", "Smith")
+    create_actor("Jaden", "Smith")
+    create_actor("Scarlett", "Keegan")
+    create_actor("Scarlett", "Johansson")
+
+    # Update genre Dramma -> Drama
+    dramma = Genre.objects.get(name="Dramma")
+    update_genre(dramma.id, "Drama")
+
+    # Update actors
+    george = Actor.objects.filter(
+        first_name="George",
+        last_name="Klooney"
+    ).first()
+    update_actor(
+        george.id,
+        first_name="George",
+        last_name="Clooney"
+    )
+
+    kianu = Actor.objects.filter(
+        first_name="Kianu",
+        last_name="Reaves"
+    ).first()
+    update_actor(kianu.id, first_name="Keanu", last_name="Reeves")
+
+    # Delete genre Action
+    action = Genre.objects.filter(name="Action").first()
+    delete_genre(action.id)
+
+    # Delete all Scarletts
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # Повертаємо QuerySet акторів із прізвищем "Smith"
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+
+if __name__ == "__main__":
+    main()
--- settings.py
+++ settings.py
@@ -18,3 +18,7 @@ DATABASES = {
 USE_TZ = False
 
 INSTALLED_APPS = ("db",)
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
